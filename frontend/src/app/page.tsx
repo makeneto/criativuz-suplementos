580504b068fd28b9c93795d402f83baea0b2ebbf
Makene Neto <makenewise@gmail.com>
2025-11-02 09:26:32 +0100

    initial commit

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,10 @@
 import HeroSection from "@/components/HeroSection"
 import BrandsSection from "@/components/BrandsSection"
 import Steps from "@/components/Steps"
-import Bestsellers from "@/components/Bestsellers"
 import AboutSection from "@/components/AboutSection"
 import Gallery from "@/components/Gallery"
 import EndSection from "@/components/EndSection"
+import Bestsellers from "../components/Bestsellers"
 
 export default async function Home() {
     return (
