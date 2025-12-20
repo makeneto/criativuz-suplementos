4861eb1222e9b3abd29584e8313509f90dcd7b7c
Makene Neto <makenewise@gmail.com>
2025-12-20 16:26:30 +0100

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,6 @@ import cors from "cors"
 import dotenv from "dotenv"
 import fs from "fs"
 import path from "path"
-import { fileURLToPath } from "url"
 
 dotenv.config()
 
@@ -11,17 +10,8 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-
-let data
-try {
-    const filePath = path.join(__dirname, "data.json")
-    data = JSON.parse(fs.readFileSync(filePath, "utf-8"))
-} catch (error) {
-    console.error("Erro ao carregar data.json:", error)
-    data = { all: [] }
-}
+const filePath = path.join(process.cwd(), "./data.json")
+const data = JSON.parse(fs.readFileSync(filePath, "utf-8"))
 
 app.get("/", (req, res) => {
     res.send("API Criativuz Suplementos rodando 🚀")
